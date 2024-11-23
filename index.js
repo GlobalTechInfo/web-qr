b92bb476dea4d0522da57c29279e2aa508caa82a
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-11-23 13:57:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -93,7 +93,8 @@ if (fs.existsSync('./session')) {
                         return `${result}${number}`;
                         }
 
-                        const mega_url = await upload(fs.createReadStream(_dirname+'creds.json'), `${randomMegaId()}.json`);
+                        const auth_path = './session/';
+                        const mega_url = await upload(fs.createReadStream(auth_path + 'creds.json'), `${randomMegaId()}.json`);
 
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
 
